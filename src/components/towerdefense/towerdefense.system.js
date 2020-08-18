83ebf2d41002265582e0564d3f148cb84fd2a482
Timmy Kokke <mental_c@hotmail.com>
2020-08-18 18:27:45 +0200

    more dynamic level

--- src/components/towerdefense/towerdefense.system.js
+++ src/components/towerdefense/towerdefense.system.js
@@ -1,13 +1,16 @@
 AFRAME.registerSystem('td-tower-defense', {
     schema: {
-        page:{
-            type:'selector'
+        page: {
+            type: 'selector'
         },
-        start:{
-            type:'selector'
+        start: {
+            type: 'selector'
         },
-        container:{
-            type:'selector'
+        container: {
+            type: 'selector'        
+        },
+        placeholder: {
+            type: 'selector'
         }
 
     },
@@ -20,19 +23,41 @@ AFRAME.registerSystem('td-tower-defense', {
             new THREE.Vector3(25, 10, 0),
             new THREE.Vector3(0, 10, 25)
         ]
+        this.placeholders = [
+            new THREE.Vector3(2, 0, 20),
+            new THREE.Vector3(-2, 0, 20),
+            new THREE.Vector3(2, 0, 15),
+            new THREE.Vector3(-2, 0, 15),
+            new THREE.Vector3(2, 0, 10),
+            new THREE.Vector3(-2, 0, 10),
+            new THREE.Vector3(2, 0, 5),
+            new THREE.Vector3(-2, 0, 5),
+
+            new THREE.Vector3(25, 10, -2),
+            new THREE.Vector3(25, 10, 2),
+            new THREE.Vector3(25, 8, 0),
+            new THREE.Vector3(25, 12, 0),
+        ];
+
         this.update();
     },
-    update: function(){
+    update: function () {
         const spawner = this.data.start.cloneNode(true);
         spawner.setAttribute("position", this.targets[0])
         this.data.container.append(spawner);
 
         const page = this.data.page.cloneNode(true);
-        page.setAttribute("position", this.targets[this.targets.length-1])
+        page.setAttribute("position", this.targets[this.targets.length - 1])
         this.data.container.append(page);
+
+        this.placeholders.forEach(pos => {
+            const ph = this.data.placeholder.cloneNode(true);
+            ph.setAttribute("position", pos)
+            this.data.container.append(ph);
+        });
     },
     nextTarget: function (targetIndex) {
-        
+
         if (targetIndex >= 0 && targetIndex < this.targets.length) {
             return this.targets[targetIndex];
         }

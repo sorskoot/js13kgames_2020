28991f2df2ea652674d60c5b91683fb9328b036d
Sorskoot <mental_c@hotmail.com>
2020-08-25 00:18:53 +0200

    added ideas for the towers

--- src/components/entities/tower-entity.component.js
+++ src/components/entities/tower-entity.component.js
@@ -1,14 +1,15 @@
 import { createPixelMaterial } from '../../lib/PixelMaterial';
 AFRAME.registerComponent('tower-entity', {
     schema: {
-        type:{
-            default:0
+        type: {
+            default: 0
         }
     },
     init: function () {
+        this.data.type = ~~(Math.random() * 3);
         var geometry = new THREE.BoxBufferGeometry(1, 1, 1);
         const pixelMaterial = createPixelMaterial(4);
-        this.pixelMaterial2 = createPixelMaterial(10, "#ffffff", this.data.type+2);
+        this.pixelMaterial2 = createPixelMaterial(4, "#ffffff", this.data.type + 9);
         const whiteMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff })
         var materials = [
             pixelMaterial,
@@ -18,20 +19,20 @@ AFRAME.registerComponent('tower-entity', {
             pixelMaterial,
             pixelMaterial,
         ];
-        var mesh = new THREE.Mesh(geometry, materials);
+        var mesh = new THREE.Mesh(geometry, this.pixelMaterial2);
         this.el.setObject3D('mesh', mesh);
-        this.q=0.0;
-        this.t=0;
+        this.q = 0.0;
+        this.t = 0;
     },
-    update(){
-        this.pixelMaterial2.uniforms.lookupIndex.value = this.data.type+2;
+    update() {
+        this.pixelMaterial2.uniforms.lookupIndex.value = this.data.type + 9;
     },
     tick(time, deltaTime) {
-        this.t += deltaTime;
-        if (this.t > 100) {
-            this.q= (this.q-1)%5;
-            this.pixelMaterial2.uniforms.lookupShift.value = this.q;
-            this.t = 0;
-        }
+        // this.t += deltaTime;
+        // if (this.t > 100) {
+        //     this.q= (this.q-1)%5;
+        //     this.pixelMaterial2.uniforms.lookupShift.value = this.q;
+        //     this.t = 0;
+        // }
     }
 });

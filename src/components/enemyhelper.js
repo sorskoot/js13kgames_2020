5d2ec3928b9853f586262c6eb74dc3e5c03c20b9
Sorskoot <mental_c@hotmail.com>
2020-09-05 10:40:19 +0200

    removed null coalescing, terser doesn't like it

--- src/components/enemyhelper.js
+++ src/components/enemyhelper.js
@@ -7,12 +7,12 @@ function closestEnemy(position, maxDistance = 5) {
     const enemies = document.querySelectorAll('[td-enemy]');
     let closest, distance = maxDistance;
     enemies.forEach(e => {
-   
+
         // ignore any enemy that's too far away on any axis.
-        if(Math.abs(e.object3D.position.x - position.x) > maxDistance ||
-        Math.abs(e.object3D.position.y - position.y) > maxDistance ||
-        Math.abs(e.object3D.position.z - position.z) > maxDistance ) return;
-   
+        if (Math.abs(e.object3D.position.x - position.x) > maxDistance ||
+            Math.abs(e.object3D.position.y - position.y) > maxDistance ||
+            Math.abs(e.object3D.position.z - position.z) > maxDistance) return;
+
         const thisDistance = position.distanceTo(e.object3D.position);
         if (thisDistance < distance) {
             closest = e;
@@ -25,13 +25,15 @@ function closestEnemy(position, maxDistance = 5) {
     }
 }
 
-function createExplosion(el, position, color, size = .1, velocity = 32, outward = 2000, burst=5) {
-    let ent = document.createElement("a-entity");
-    ent.setAttribute("explosion", {
-        color: color, size: size,
-        velocityStart: velocity, outward: outward,
-        burst:burst
-    });
-    ent.setAttribute("position", position);
-    el.parentElement?.append(ent);
+function createExplosion(el, position, color, size = .1, velocity = 32, outward = 2000, burst = 5) {
+    if (el.parentElement) {
+        let ent = document.createElement("a-entity");
+        ent.setAttribute("explosion", {
+            color: color, size: size,
+            velocityStart: velocity, outward: outward,
+            burst: burst
+        });
+        ent.setAttribute("position", position);
+        el.parentElement.append(ent);
+    }
 }
